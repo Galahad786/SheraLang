1cff392fe3f1b60871315b08c55174e60bb1f931
Galahad786 <155619081+Galahad786@users.noreply.github.com>
2025-02-16 12:47:39 +0500

    Add files via upload

---
+++ sheralang.h
@@ -0,0 +1,32 @@
+#ifndef SHERALANG_H_
+#define SHERALANG_H_
+
+#include <iostream>
+
+#define shuruKrenShera main()
+
+#define iyehai int
+#define syehai string
+#define cyehai character
+#define byehai bool
+#define fyehai float
+#define dyehai double
+#define lyehai long
+#define ddyehai double double
+
+#define dikhadoshera std::cout<<
+#define leloshera std::cin>>
+#define agar if
+#define warna else
+#define yaphir else if
+#define choosekroshera switch
+#define yehoskta case
+#define mera_Boss_Sabse_Upar return
+#define neechayJaoShera <<std::endl;
+#define warnaYehHai default:
+
+#define chaloShera for
+#define dekhoshera while
+#define torDoShera break;
+
+#endif // CUSTOM_MACROS_H
